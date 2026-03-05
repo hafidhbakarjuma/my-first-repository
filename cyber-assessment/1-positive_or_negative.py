a25ca5da0a6996cb61c00e131306834a7a20ed4a
hafidhbakarjuma <hafidhbakar504@gmail.com>
2026-03-05 14:08:22 +0100

    Modfied the 1-positive_or_negative

--- cyber-assessment/1-positive_or_negative.py
+++ cyber-assessment/1-positive_or_negative.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10, 10)
+number = 0
 
-if number > 0:
-    print(f"{number} is positive")
-elif number == 0:
-    print(f"{number} is zero")
-else:
-    print(f"{number} is negative")
+def check_number():
+    if number > 0:
+        print(f"{number} is positive")
+    elif number == 0:
+        print(f"{number} is zero")
+    else:
+        print(f"{number} is negative")
+
+
+if __name__ == "__main__":
+    number = random.randint(-10, 10)
+    check_number()
