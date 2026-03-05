64f8a26d01c745c051e002102390ea9b4eee9b27
hafidhbakarjuma <hafidhbakar504@gmail.com>
2026-03-05 13:27:28 +0100

    cyber-assessment update

---
+++ cyber-assessment/5-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    data = response.json()
+    print(data.get("id"))
